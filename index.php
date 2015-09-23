e24cf0fc71159027c92ccebf004119f9f2aff8a8
joyabia <joyabia@g.harvard.edu>
2015-09-23 07:43:25 -0700

    fix to return php password value

--- index.php
+++ index.php
@@ -29,70 +29,87 @@ session_start();
 </head>
 <body>
 <div class="container">
-  <h1 id="header">  Joy's XKCD Password Generator<h1>
-  <div class="col-md-4">
+<!--this is the heading of the document-->
+  <h1 id="header">  Joy's XKCD Password Generator</h1>
+<!--end heading-->
+
+<!--this is the sidebar where I explain how the xkcd generator works-->
+  <div class="col-md-4" id="section-left">
   <div id="box">
-	<h3 id="header_sidebar"> How Joy's XKCD Password Generator Works<h3>
+	<h3 id="header_sidebar"> How Joy's XKCD Password Generator Works</h3>
   <p id="text_sidebar">The concept of XKCD passwords, is to generate passwords that
   are easier for humans to remember but more difficult for computers to crack.  
   Difficult passwords translates to written down passwords which pose security risks.</p>
   <p> An example of the relative strength of xkcd passwords compared to other forms
-  can be seen here: <a href="https://xkcd.com/936/">Relative Strength of XKCD passwords </a>
+  can be seen here: <a href="https://xkcd.com/936/">Relative Strength of XKCD passwords </a></p>
 
   <p>The XKCD Password generator, generates a password  that is a combination of english words. 
   The user choses the Number of words by entering a number in the field "Number of Words". 
   The user elects to include special character separators. The user can also decide to 
   append a number to the password. </p>
   </div><!--close box-->
-  </div><!--close col-sm-3-->
+  </div><!--close box-sidebar-left-->
+<!--close section-left-->
+
+<!--form section-->
   <div class="col-md-6" >
   <div class="row" id="password_gen">
 <form class="form-horizontal" action="logic.php" method="get">
+
+
   <div class="form-group">
-    Generated Password:
-    <input type="text" class="form-control" name="gen_password" value="<?= $_SESSION['gen_password']?>" id="gen_password" placeholder="copy generated password from here" readonly>
+    <label class="label-class">Generated Password:</label>
+    <input type="text" class="form-control" name="gen_password" value="<?php echo $_SESSION['gen_password']?>" id="gen_password" placeholder="copy generated password from here" readonly>
   
-  </div>
+  </div><!--close div for gen password field-->
+
   <div class="form-group">
-   Number of words: <input type="text" class="form-control" name="numwords" id="numwords" placeholder="4">
-  </div>
-
-
+   
+<label class="label-class"> Number of words:</label>
+<input type="text" class="form-control" name="numwords" id="numwords" placeholder="3" onfocus="this.placeholder='';">
+  </div><!--close div for number of words field-->
 
 
 <!--include a special character-->
   <div class="form-group">
- Include a separator (+, #, -,...):
- <select class="form-control" name="separator">
-<option></option>
-  <option>-</option>
-  <option>+</option>
-  <option>$</option>
-  <option>=</option>
-  <option>%</option>
-  <option>#</option>
-</select>
-</div>
+    <label class="label-class"> Include a separator (+, #, -,...):</label>
+    <select class="form-control" name="separator">
+      <option>&nbsp;</option>
+      <option>-</option>
+      <option>+</option>
+      <option>$</option>
+      <option>=</option>
+      <option>%</option>
+      <option>#</option>
+    </select>
+  </div>
 
   <!--Ask if user wants a number appended to the password -->
-    <div class="form-group">
-<div class="checkbox">
+ <div class="form-group">
+ <div class="checkbox">
   <label class="label-class">
     <input type="checkbox" name="AppendNum">
     Append a number(1-10)?
   </label>
 </div>
+</div>
 
-</div>
-  </div><!--close row-->
-<div id="button_div">
+
+<div class ="row" id="button_div">
   <button type="submit" class="btn btn-primary">Submit</button>
-  </div>
+</div><!--close button div-->
+
 </form>
-</div>
+
+</div><!-- close row form section-->
+
+
   <div class="col-md-2">
   </div><!--close col-sm-2-->
+  
 </div><!--close container class-->
-	
+
+
+</div>
 </body>
 </html>
